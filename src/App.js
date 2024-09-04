6a3d6ff775b9f1cd6b76719d04f32733e53c0e0e
JayMurphy45 <B00143682@mytudublin.ie>
2024-09-04 09:19:23 +0100

    Validating and submitting images
    added useMemo hook to add a image to the gallery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import UploadForm from "./components/UploadForm";
 
 const photos = [
   "https://picsum.photos/id/1001/200/200",
@@ -12,14 +14,59 @@ const photos = [
 ];
 
 function App() {
+  const [count, setCount] = useState("");
+  const [inputs, setInputs] = useState({ title: null, file: null, path: null });
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, collapse] = useState(false);
+
+  const toggle = () => collapse(!isCollapsed);
+  const handleOnChange = (e) => {
+    if (e.target.name === "file") {
+      setInputs({
+        ...inputs,
+        file: e.target.files[0],
+        path: URL.createObjectURL(e.target.files[0]),
+      });
+    } else {
+      setInputs({ ...inputs, title: e.target.value });
+    }
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (inputs.title && inputs.title.trim() && inputs.path) {
+      setItems([inputs.path, ...items]);
+      setInputs({ title: "", file: null, path: null }); // Reset input
+    } else {
+      console.error("Please fill out the title and upload a file.");
+    }
+    setInputs({ title: null, file: null, path: null });
+    collapse(false);
+  };
+
+  useEffect(() => {
+    setCount(`you have ${items.length} image${items.length > 1 ? "s" : ""}`);
+  }, items);
+
   return (
     <>
       <Navbar />
-      <div class="container text-center mt-5">
+
+      <div className="container text-center mt-5">
+        <button className="btn btn-success float-end" onClick={toggle}>
+          {isCollapsed ? "close" : "+Add"}
+        </button>
+        <div className="clearfix mb-4"></div>
+        <UploadForm
+          inputs={inputs}
+          isVisible={isCollapsed}
+          onChange={handleOnChange}
+          onSubmit={handleOnSubmit}
+        />
+        {count}
         <h1>Gallery</h1>
-        <div class="row mt-5">
-          {photos.map((photo) => (
-            <Card src={photo} />
+        <div className="row mt-5">
+          {items.map((photo, index) => (
+            <Card key={index} src={photo} />
           ))}
         </div>
       </div>
